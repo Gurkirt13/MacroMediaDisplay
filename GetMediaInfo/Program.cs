40651e5d965d055b6a9dfafa120d137d0edad225
WafferX(L) <gurkirtsingh1313@gmail.com>
2024-06-12 19:48:45 -0400

    Initial commit
    it has working app that gets the media data and send it on serial and take serial input to modify the per app audio

---
+++ GetMediaInfo/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Media.Control;
+using Windows.Media;
+using System.IO.Ports;
+using AudioSwitcher.AudioApi.CoreAudio;
+using NAudio.CoreAudioApi;
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
+
+
+public static class Program
+{   
+    //store and compare session info
+    private static GlobalSystemMediaTransportControlsSession currentSession;
+    //Audio controller for changing each app audio individually
+    //private static CoreAudioController audioController = new CoreAudioController();
+    //serial port for communication between arduino and PC
+    private static SerialPort serialPort;
+
+    private static MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
+    private static MMDevice defaultPlaybackDevice;
+
+
+    public static async Task Main(string[] args)
+    {
+        var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
+        currentSession = gsmtcsm.GetCurrentSession();
+
+
+        defaultPlaybackDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+        
+
+        // Initialize the SerialPort
+        serialPort = new SerialPort("COM3", 9600); // Replace COM3 with your Arduino's COM port
+        serialPort.Parity = Parity.None;
+        serialPort.StopBits = StopBits.One;
+        serialPort.DataBits = 8;
+        serialPort.Handshake = Handshake.None;
+        serialPort.RtsEnable = true;
+        serialPort.Open();
+
+        //settings serial port data reciving binding to a event so we can work on it.
+        serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+        if (currentSession == null)
+        {
+            Console.WriteLine("No active media session found.");
+        }
+        else
+        {
+            // Subscribe to the media properties changed event
+            currentSession.MediaPropertiesChanged += MediaPropertiesChanged;
+            await DisplayCurrentMediaProperties();
+        }
+
+        Console.WriteLine("Press any key to quit..");
+        Console.ReadKey(true);
+    }
+
+    private static async Task<GlobalSystemMediaTransportControlsSessionManager> GetSystemMediaTransportControlsSessionManager()
+    {
+        try
+        {
+            return await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to get media transport controls session manager: " + ex.Message);
+            throw;
+        }
+    }
+
+    private static async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaProperties(GlobalSystemMediaTransportControlsSession session)
+    {
+        try
+        {
+            return await session.TryGetMediaPropertiesAsync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to get media properties: " + ex.Message);
+            throw;
+        }
+    }
+
+    private static async void MediaPropertiesChanged(GlobalSystemMediaTransportControlsSession session, MediaPropertiesChangedEventArgs args)
+    {
+        await DisplayCurrentMediaProperties();
+    }
+
+    private static async Task DisplayCurrentMediaProperties()
+    {
+        var mediaProperties = await GetMediaProperties(currentSession);
+        if (mediaProperties != null)
+        {
+          
+            string SerialSend = $"{mediaProperties.Artist} - {mediaProperties.Title}";
+            Console.WriteLine(SerialSend);
+            
+            serialPort.WriteLine(SerialSend);
+            
+
+        }
+        else
+        {
+            Console.WriteLine("Failed to retrieve media properties.");
+        }
+    }
+
+    //process the serial data to filter it and in future use for other things then just volume control
+    private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+    {
+        //Reading the serial data
+        string inData = serialPort.ReadLine();
+        
+
+        //Actual data filtering for each volume input
+        if (inData.StartsWith("Volume1:"))
+        {
+            string volumeString = inData.Substring(8).Trim();
+            if (int.TryParse(volumeString, out int volume))
+            {
+                SetApplicationVolume("Chrome", volume);
+            }
+        }
+       /* else if (inData.StartsWith("Volume2:"))
+        {
+            string volumeString = inData.Substring(8).Trim();
+            if (int.TryParse(volumeString, out int volume))
+            {
+                SetApplicationVolume("Spotify", volume);
+            } 
+        } */
+    }
+
+    //Set application vomume here
+    private static void SetApplicationVolume(string appName, int volume)
+    {
+        
+        // var sessions = audioController.DefaultPlaybackDevice.SessionController.AllSessions();
+        var sessions = defaultPlaybackDevice.AudioSessionManager.Sessions;
+
+        for (int i = 0; i < sessions.Count; i++)
+        {
+            var session = sessions[i];
+            //session.ToString
+            if (session.GetSessionIdentifier.Contains(appName))
+            {
+                session.SimpleAudioVolume.Volume = volume / 100.0f;
+                Console.WriteLine($"Set {appName} volume to {volume}");
+                break;
+            }
+        }
+    }
+}
---
+++ MVDC/MVDC.ino
@@ -0,0 +1,86 @@
+#include <GxEPD2_3C.h>
+#define GxEPD2_DISPLAY_CLASS GxEPD2_3C
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+#define GxEPD2_DRIVER_CLASS GxEPD2_290_Z13c // GDEH029Z13  128x296, UC8151D, (HINK-E029A10-A3 20160809)
+
+#define MAX_DISPLAY_BUFFER_SIZE 800 // 
+#define MAX_HEIGHT(EPD) (EPD::HEIGHT <= (MAX_DISPLAY_BUFFER_SIZE / 2) / (EPD::WIDTH / 8) ? EPD::HEIGHT : (MAX_DISPLAY_BUFFER_SIZE / 2) / (EPD::WIDTH / 8))
+GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, MAX_HEIGHT(GxEPD2_DRIVER_CLASS)> display(GxEPD2_DRIVER_CLASS(/*CS=*/ 10, /*DC=*/ 8, /*RST=*/ 9, /*BUSY=*/ 7));
+
+int16_t tbx, tby; uint16_t tbw, tbh;
+String receivedData;
+int temp1;
+void setup()
+{
+  //display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
+  display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
+  
+  pinMode(4, OUTPUT); //Test LED
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b); 
+  //StartSerialPort
+  Serial.begin(9600); //
+
+  pinMode(18, INPUT);
+
+}
+
+void loop() {
+
+  //VolumeControl
+  int slider1Value = analogRead(18); // Read value from slider 1
+  int slider2Value = analogRead(3); // Read value from slider 2
+
+  // Map the slider values to a range from 0 to 100
+  int volume1 = map(slider1Value, 0, 1023, 0, 100);
+ 
+  int volume2 = map(slider2Value, 0, 1023, 0, 100);
+  if (volume1!=temp1)
+  {
+    Serial.print("Volume1:"); Serial.println(volume1);
+    temp1=volume1;
+  }
+
+ 
+
+ 
+  //UpdateSongs
+  if (Serial.available() > 0) 
+  { // Check if data is available to read
+    // Clear the serial buffer to ensure we process the latest data
+    
+    while (Serial.available() > 0) {
+      receivedData = Serial.readStringUntil('\n'); // Read the incoming string
+    }
+
+    // Only update the display if the song title has changed
+    static String previousSong = "";
+    if (receivedData != previousSong) {
+      digitalWrite(4, HIGH);
+      updateDisplay(receivedData);
+      previousSong = receivedData;
+    }
+  }
+}
+
+void updateDisplay(String songTitle) 
+{
+        display.getTextBounds(songTitle, 0, 0, &tbx, &tby, &tbw, &tbh);
+        uint16_t x = ((display.width() - tbw) / 2) - tbx;
+        uint16_t y = ((display.height() - tbh) / 2) - tby;
+        display.setFullWindow();
+        //display.setPartialWindow( 48, 92, 48, 92);
+        //display.fillScreen(GxEPD_WHITE);
+        display.setTextColor(GxEPD_BLACK);
+        display.firstPage();
+        do
+          {
+            display.setCursor(x, y);
+            display.print(songTitle);
+            display.display(false);
+          }
+        while (display.nextPage());
+        //display.hibernate(); 
+        receivedData=songTitle;
+}
